8de58b23a1ea1582bb38fd117f59a344fbc119e4
Benjie Cheong <benjie1812@gmail.com>
2019-11-02 13:35:12 +0800

    [Chore] Clean up directory

---
+++ Ferdora Package Downloader/README.md
@@ -0,0 +1,3 @@
+Only works when the offline env and the online env is at the same state
+
+Expect package argument
--- Ferdora Package Downloader/download-fedora-packages.sh
+++ Ferdora Package Downloader/download-fedora-packages.sh
@@ -5,9 +5,9 @@ DOWNLOADDIR=~/Downloads/Packages
 ZIPDIR=~/Downloads
 sudo rm "$ZIPDIR/Packages.zip"
 mkdir -p $DOWNLOADDIR
-sudo dnf download --resolve --arch x86_64 --arch noarch --downloaddir $DOWNLOADDIR $1
+sudo dnf download --resolve --arch noarch --arch x86_64 --downloaddir $DOWNLOADDIR $1
 echo "Zipping files for transfer to disconencted environment"
 zip -j "$ZIPDIR/Packages.zip" $DOWNLOADDIR/*
 echo "CHECKSUM: $(cksum "$ZIPDIR/Packages.zip")"
 sudo rm -rf $DOWNLOADDIR
-xdg-open $ZIPDIR
+nautilus $ZIPDIR &
